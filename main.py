fab3bf498b8b37c1760d83a739be1e8d3aa93b3a
Igor Magur <magur@live.ru>
2018-09-12 16:22:12 +0300

    Removed everything to game flow

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import os
 import pygame
 
 import modules.constants as c
-from modules.flow_handlers import game_flow_handler, score_flow_handler
 from modules.game import Game
-from modules.input_handler import end_game_state_handler
 from modules.board import Board
-from modules.helpers import write_end_text, enter_player_name, draw_rect, get_board_color, EndGame, get_not_scored
+from modules.helpers import EndGame
 
 pygame.init()
 pygame.key.set_repeat(300, 50)  # 300 pause before repeat
@@ -30,54 +28,11 @@ def main():
 
     myfont = pygame.font.SysFont("monospace", 16)
     end_game = EndGame(myfont, c.WHITE, size, screen, board)
-    game = Game(board, pygame, screen, end_game)
+    game = Game(board, pygame, screen, end_game, block_bg, myfont)
     game.init_timer()
     screen.fill(c.GRAY)
     while True:
-        if game.get_state() == game.GAME_STATE:
-            """ Game running state """
-            for event in pygame.event.get():
-                game_flow_handler(event, pygame, board, game)
-
-            if not game.is_paused():
-                game.board_surf.fill(c.BLACK)
-                for coords in board.print_board():
-                    draw_rect(game.board_surf, get_board_color(board.get_fullness()), coords, block_bg)
-                for coords in board.print_figure():
-                    draw_rect(game.board_surf, board.figure.get_color(), coords, block_bg)
-            else:
-                write_end_text(screen, [myfont.render('Paused', 1, c.WHITE)], size)
-
-            scoretext = myfont.render(board.score.get_score(), 1, c.WHITE)
-            game.board_surf.blit(scoretext, (5, 10))
-            if board.score.get_int_score() >= 100 * game.get_round():
-                game.next_round()
-            pygame.display.flip()
-        elif game.get_state() == game.SCORE_STATE:
-            """ Show and input score """
-            screen.fill(c.BLACK)
-
-            if not board.score.check_score():
-                text = get_not_scored(myfont, board, c.WHITE)
-            else:
-                text = enter_player_name(myfont, board, c.WHITE)
-            write_end_text(screen, text, size)
-
-            for event in pygame.event.get():
-                score_flow_handler(event, pygame, board)
-
-            pygame.display.flip()
-
-        elif game.get_state() == game.ENDGAME_STATE:
-            screen.fill(c.BLACK)
-
-            end_game.draw()
-            for event in pygame.event.get():
-                end_game_state_handler(event, pygame, game)
-            pygame.display.flip()
-        else:
-            # TODO: Add watch score screen in menu
-            break
+        game.flow()
 
 
 if __name__ == '__main__':
--- modules/game.py
+++ modules/game.py
@@ -1,4 +1,7 @@
 from modules import constants as c
+from modules.flow_handlers import game_flow_handler, score_flow_handler
+from modules.helpers import draw_rect, get_board_color, write_end_text, get_not_scored, enter_player_name
+from modules.input_handler import end_game_state_handler
 
 
 class Game:
@@ -6,7 +9,7 @@ class Game:
     SCORE_STATE = 2
     ENDGAME_STATE = 3
 
-    def __init__(self, board, pygame, screen, end_game):
+    def __init__(self, board, pygame, screen, end_game, block_bg, myfont):
         self.board = board
         self.pygame = pygame
         self.screen = screen
@@ -20,6 +23,8 @@ class Game:
         # Needed to break keypress repeat
         self.k_down_pressed = False
         self.round = 1
+        self.block_bg = block_bg
+        self.font = myfont
 
     def get_state(self):
         if self.board.get_game_state():
@@ -67,8 +72,56 @@ class Game:
     def init_timer(self):
         self.pygame.time.set_timer(c.MOVEDOWN, self.speed)
 
-    def game_flow(self):
-        pass
+    def _gameplay_flow(self):
+        """ Game running state """
+        for event in self.pygame.event.get():
+            game_flow_handler(event, self.pygame, self.board, self)
+
+        if not self.is_paused():
+            self.board_surf.fill(c.BLACK)
+            for coords in self.board.print_board():
+                draw_rect(self.board_surf, get_board_color(self.board.get_fullness()), coords, self.block_bg)
+            for coords in self.board.print_figure():
+                draw_rect(self.board_surf, self.board.figure.get_color(), coords, self.block_bg)
+        else:
+            write_end_text(self.screen, [self.font.render('Paused', 1, c.WHITE)], self.screen.get_size())
+
+        scoretext = self.font.render(self.board.score.get_score(), 1, c.WHITE)
+        self.board_surf.blit(scoretext, (5, 10))
+        if self.board.score.get_int_score() >= 100 * self.get_round():
+            self.next_round()
+        self.pygame.display.flip()
+
+    def _score_flow(self):
+        """ Show and input score """
+        self.screen.fill(c.BLACK)
+
+        if not self.board.score.check_score():
+            text = get_not_scored(self.font, self.board, c.WHITE)
+        else:
+            text = enter_player_name(self.font, self.board, c.WHITE)
+        write_end_text(self.screen, text, self.screen.get_size())
+
+        for event in self.pygame.event.get():
+            score_flow_handler(event, self.pygame, self.board)
+
+        self.pygame.display.flip()
+
+    def _menu_flow(self):
+        self.screen.fill(c.BLACK)
+
+        self.end_game.draw()
+        for event in self.pygame.event.get():
+            end_game_state_handler(event, self.pygame, self)
+        self.pygame.display.flip()
+
+    def flow(self):
+        if self.get_state() == self.GAME_STATE:
+            self._gameplay_flow()
+        elif self.get_state() == self.SCORE_STATE:
+            self._score_flow()
+        elif self.get_state() == self.ENDGAME_STATE:
+            self._menu_flow()
 
     def reset(self):
         self.screen.fill(c.GRAY)
